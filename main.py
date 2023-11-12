df3b9d8f6cd7abb16d9e6fe4e050a4afdbc340e6
romain_barraud <romain.barraud@bolttech.io>
2023-11-12 21:52:30 +0800

    stopped tracking README.md

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cnnClassifier import logger
 from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 
 # STAGE_NAME = "Data Ingestion stage"
@@ -13,12 +14,24 @@ from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseMo
 #     logger.exception(e)
 #     raise e
 
-STAGE_NAME = "Prepare base model"
+# STAGE_NAME = "Prepare base model"
+# try:
+#     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+#     obj = PrepareBaseModelTrainingPipeline()
+#     obj.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+STAGE_NAME = "Training"
 try:
+    logger.info(f"*******************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
+        
